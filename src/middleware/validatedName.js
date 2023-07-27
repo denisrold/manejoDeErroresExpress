b3cefe4dd357b5df93b1a8c1ec28b99a2665659d
Denis H. Roldán <112777347+denisrold@users.noreply.github.com>
2023-07-27 12:53:35 -0300

    Finish manejo de errroes

--- src/middleware/validatedName.js
+++ src/middleware/validatedName.js
@@ -3,6 +3,7 @@ const { ClientError } = require("../utils/errors");
 const validatedName = (req, res, next) => {
   const { name } = req.query;
   console.log(name);
+  //Aca lasvalidaciones serancon regex
   if (name && name.includes("@")) throw new ClientError("Invalid Name", 400);
   next();
 };
