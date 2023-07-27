5421576efa88dace9d7f5622936cbfd1e01dbff1
Denis H. Roldán <112777347+denisrold@users.noreply.github.com>
2023-07-27 17:35:55 -0300

    Validation

--- src/middleware/validatedName.js
+++ src/middleware/validatedName.js
@@ -1,5 +1,5 @@
 const { ClientError } = require("../utils/errors");
-
+//Estas validaciones las paso en el index.js de routes como midleware antes de la funcion para querevise si la info esta correcta
 const validatedName = (req, res, next) => {
   const { name } = req.query;
   console.log(name);
